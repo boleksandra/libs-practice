6e4243a79c417828b6463ba04f0a69351b907026
oleksandra <oleksandrabodiak@gmail.com>
2026-04-08 17:46:41 +0300

    practice12

---
+++ graphs.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+#
+# days = ['Пн', 'Вв', 'Ср', 'Чт', 'Пт']
+# coffee_sales = [45, 50, 42, 60, 85]
+#
+# plt.figure(figsize=(8, 4)) # figsize=(ширина, висота) в дюймах
+# plt.plot(days, coffee_sales, color='blue', marker='o', linestyle='--')
+# plt.title("Тренд продажів кави")
+# plt.xlabel("Дні тижня")
+# plt.ylabel("Кількість чашок")
+# plt.grid(True) # Вмикаємо сітку
+# plt.show() # Відображаємо результат
+# drinks = ['Еспресо', 'Латте', 'Капучино']
+# popularity = [120, 300, 250]
+#
+# plt.figure(figsize=(8, 4))
+# plt.bar(drinks, popularity, color=['#8B4513', '#DEB887', '#D2B48C'])
+# plt.title("Популярність напоїв")
+# plt.ylabel("Кількість замовлень")
+# plt.show()
+# hours_studied = [1, 2, 3, 4, 5, 6]
+# exam_scores = [40, 50, 65, 70, 85, 90]
+#
+# plt.figure(figsize=(8, 4))
+# plt.scatter(hours_studied, exam_scores, color='red', s=1000, alpha=0.9)
+# plt.plot(days, coffee_sales, color='blue', marker='o', linestyle='--')
+# plt.title("Залежність балів від часу підготовки")
+# plt.xlabel("Години підготовки")
+# plt.ylabel("Бал за іспит")
+# plt.show()
+
+
+
--- pandas_plt_ps.py
+++ pandas_plt_ps.py
@@ -1,23 +1,29 @@
 import kagglehub
 import pandas as pd
-
+import matplotlib.pyplot as plt
 # kagglehub.dataset_download("alitaqishah/global-mental-health-crisis-index-2026", output_dir="datasets", force_download=True)
 df = pd.read_csv("datasets/Global_Mental_Health_Crisis_Index_2026.csv")
 # print(df.head())
-# depression = df[["depression_pct", "country"]]
-# print(depression)
-# mask = (df['region'] == 'Europe') & (df['depression_pct'] >= 5)
-# europe_depression = df[mask]
-# print(europe_depression [[ "country", "depression_pct"]])
+# # depression = df[["depression_pct", "country"]]
+# # print(depression)
+# # mask = (df['region'] == 'Europe') & (df['depression_pct'] >= 5)
+# # europe_depression = df[mask]
+# # print(europe_depression [[ "country", "depression_pct"]])
+#
+# # df["millions_mul_rate"] = df["suicide_rate_per100k"] * df["population_millions"] * 10
+# # print(df[["country", "millions_mul_rate"]])
+# gdp_geq_40k = df[df["gdp_per_capita_usd"]>=4000]
+# gdp_geq_40k_low_help = gdp_geq_40k[gdp_geq_40k["mh_system_score"]<5]
+# gdp_geq_40k_low_help["lack_of_care_mln"] = (gdp_geq_40k_low_help["populattion_millions"]
+# gdp_geq_40k_low_help["treatment_gap_pct"] / 100)
+# print(gdp_geq_40k_low_help)
 
-# df["millions_mul_rate"] = df["suicide_rate_per100k"] * df["population_millions"] * 10
-# print(df[["country", "millions_mul_rate"]])
-gdp_geq_40k = df[df["gdp_per_capita_usd"]>=4000]
-gdp_geq_40k_low_help = gdp_geq_40k[gdp_geq_40k["mh_system_score"]<5]
-gdp_geq_40k_low_help["lack_of_care_mln"] = (gdp_geq_40k_low_help["populattion_millions"]
-gdp_geq_40k_low_help["treatment_gap_pct"] / 100)
-print(gdp_geq_40k_low_help)
-
-
-
-
+top5 = df.nlargest(5, 'depression_pct')
+plt.figure(figsize=(6,7))
+plt.plot(top5['country'], top5['depression_pct'], marker='o', label='depression', linewidth=2)
+plt.plot(top5['country'], top5['anxiety_pct'], marker='s', label='anxiety')
+plt.title('anxiety vs depression (%)')
+plt.xlabel('countrys')
+plt.ylabel('%')
+plt.legend()
+plt.show()
